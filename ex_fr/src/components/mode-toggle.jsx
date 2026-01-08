5c506ab6fcf994a53ae914c9578f2b1d64b8d57a
zhuoqinyue <1403450829@qq.com>
2026-01-08 10:47:55 +0800

    feat: 使用图标

--- ex_fr/src/components/mode-toggle.jsx
+++ ex_fr/src/components/mode-toggle.jsx
@@ -1,5 +1,5 @@
-// import { Moon, Sun } from 'lucide-react'
-
+import { Moon, Sun } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,16 +8,31 @@ import {
 } from '@/components/ui/dropdown-menu.jsx'
 import { useTheme } from '@/components/providers/theme-provider.jsx'
 
+const SunIcon = () => {
+  return (
+    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+  )
+}
+
+const MoonIcon = () => {
+  return (
+    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+  )
+}
 export default function ModeToggle() {
   const { setTheme, theme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <div className="capitalize">
-          {theme}
-          <span className="sr-only">Toggle theme</span>
-        </div>
+        <span role="button" tabIndex={0} className="inline-flex">
+          <Button variant="outline" size="icon">
+            {/* {theme} */}
+            <SunIcon />
+            <MoonIcon />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </span>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem
